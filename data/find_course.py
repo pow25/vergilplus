10a32f049b6a258c119d6a5e2c6b9424e78e1fd6
Pow25 <pow25@users.noreply.github.com>
2018-10-27 21:54:30 -0400

    delete the duplicated course is CSV

--- data/find_course.py
+++ data/find_course.py
@@ -1,30 +1,34 @@
 import numpy
 
-file1 = "courses.txt"
-file2 = "track_courses.txt"
-
-list1 = set()
-list2 = set()
+# file1 = "courses.txt"
+# file2 = "track_courses.txt"
+file1 = "a.txt"
+list1 = []
+# list2 = set()
 
 with open(file1) as f:
     line = f.readline()
     while line:
-        temp_line = line.rstrip('\n')
-        list1.add(temp_line)
+        
+        if line in list1:
+            print(line)
+        
+        list1.append(line)
+
         line = f.readline()
 
-with open(file2) as f:
-    line = f.readline()
-    while line:
-        temp_line = line.rstrip('\n')
-        list2.add(temp_line)
-        line = f.readline()
+# with open(file2) as f:
+#     line = f.readline()
+#     while line:
+#         temp_line = line.rstrip('\n')
+#         list2.add(temp_line)
+#         line = f.readline()
 
-diff = []
+# diff = []
 
-for i in list1:
-    if i not in list2:
-        diff.append(i)
+# for i in list1:
+#     if i not in list2:
+#         diff.append(i)
 
-print(diff)
+# print(diff)
         
