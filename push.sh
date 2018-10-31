c9f2cfe65c83bc0dcfbcb596a8a13dfbdbac32b9
Pow25 <pow25@users.noreply.github.com>
2018-10-31 01:11:29 -0400

    try push.sh

--- push.sh
+++ push.sh
@@ -7,14 +7,14 @@ setup_git() {
 
 commit_website_files() {
   git checkout -b report_files
-  git add ./target/surefire-reports/com.vplus.maven.vplus.AppTest.txt
-  git add ./target/surefire-reports/TEST-com.vplus.maven.vplus.AppTest.xml
+  git add -f ./target/surefire-reports/com.vplus.maven.vplus.AppTest.txt
+  git add -f ./target/surefire-reports/TEST-com.vplus.maven.vplus.AppTest.xml
   git commit --message "Travis uploading files"
 }
 
 upload_files() {
-  git remote add origin-pages https://${GITHUB}@github.com/pow25/vegilplus.git > /dev/null 2>&1
-  git push --quiet --set-upstream origin-pages gh-pages 
+  git remote add origin-pages https://$GITHUB@github.com/pow25/vegilplus.git > /dev/null 2>&1
+  git push --quiet --set-upstream origin-pages report_files
 }
 
 setup_git
