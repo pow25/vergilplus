1a66761ba238aca3be1272810244b5c3bc389505
Sylvia0696 <42750153+Sylvia0696@users.noreply.github.com>
2018-10-30 23:50:12 -0400

    delete note for test_app_run

--- src/test/java/com/vplus/maven/vplus/AppTest.java
+++ src/test/java/com/vplus/maven/vplus/AppTest.java
@@ -60,13 +60,13 @@ extends TestCase
 //	
 //	
 //	
-//	public void test_app_run() throws Exception {
-//		Application app  = ctx.getBean("Application", Application.class);	    
-//	    final SystemOutRule systemOutRule = new SystemOutRule().enableLog();    
-//	    app.run();
-//        
-//	    assertNotNull(systemOutRule.getLog());
-//	}
+	public void test_app_run() throws Exception {
+	    Application app  = ctx.getBean("Application", Application.class);	    
+	    final SystemOutRule systemOutRule = new SystemOutRule().enableLog();    
+	    app.run();
+       
+	    assertNotNull(systemOutRule.getLog());
+	}
 		
 		
 	
