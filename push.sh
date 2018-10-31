dd867ebeb9fdce2d19e2c26ab28d327980d2c76a
Pow25 <pow25@users.noreply.github.com>
2018-10-31 01:43:15 -0400

    change the way how files are added

--- push.sh
+++ push.sh
@@ -1,20 +1,19 @@
 #!/bin/sh
-
+mv -f ./target/surefire-reports/com.vplus.maven.vplus.AppTest.txt ./report
+mv -f ./target/surefire-reports/TEST-com.vplus.maven.vplus.AppTest.xml ./report
 setup_git() {
   git config --global user.email "zhangchi8518@gmail.com"
   git config --global user.name "pow25"
 }
 
 commit_website_files() {
-  git checkout -b report_files
-  git add -f ./target/surefire-reports/com.vplus.maven.vplus.AppTest.txt
-  git add -f ./target/surefire-reports/TEST-com.vplus.maven.vplus.AppTest.xml
-  git commit --message "Travis uploading files"
+  git checkout -b master
+  git commit --message "Travis uploading files [ci skip]"
 }
 
 upload_files() {
   git remote add origin-pages https://$GITHUB@github.com/pow25/vergilplus.git > /dev/null 2>&1
-  git push --quiet --set-upstream origin-pages report_files
+  git push --quiet --set-upstream origin-pages master
 }
 
 setup_git
