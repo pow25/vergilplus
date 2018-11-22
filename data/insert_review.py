1ee23a58ef1c8690d47df9f0a255117e7c2e756a
pow25 <zhangchi8518@gmail.com>
2018-11-22 14:56:48 -0500

    finshing the python code of insert review into database

--- data/insert_review.py
+++ data/insert_review.py
@@ -1,6 +1,32 @@
 import csv
+import pymysql
 
-with open("sentiments.csv", "r") as csvfile:
-    reader = csv.DictReader(csvfile, delimiter=",", quotechar='"')
+def change_quote(string):
+    res = ""
+    for i in string:
+        if i == "'":
+            res += "\\"
+        res += i 
+    return res
+
+cnx = pymysql.connect("localhost", "dbuser", "dbuser", "vergilplus", charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
+cursor = cnx.cursor()
+q = "CREATE TABLE `vergilplus`.`sentiment` (\
+  `number` VARCHAR(64) NOT NULL,\
+  `professor` TEXT NOT NULL,\
+  `review` TEXT NOT NULL,\
+  `score` TEXT NOT NULL,\
+  `magnitude` TEXT NOT NULL);"
+
+
+with open("sentiments.csv", "r",encoding='UTF-8') as csvfile:
+    reader = csv.DictReader(csvfile, delimiter=",")
     for i in reader:
-        print(i)
+        q = "INSERT INTO `vergilplus`.`sentiment` ( number, professor, review, score,magnitude ) VALUES ('"
+        q += i['number'] + "','"
+        q += i['professor'] + "','"
+        q += change_quote(i['review']) + "','"
+        q += i['score'] + "','"
+        q += i['magnitude'] + "');"
+        cursor.execute(q)
+        cnx.commit()
