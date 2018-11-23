d2f1c8210f788029c0d7c02a1dfacf0b0e5e9035
pow25 <zhangchi8518@gmail.com>
2018-11-22 21:21:43 -0500

    update charset of .sql for compatibility of mysql5.6

--- data/vergilplus.sql
+++ data/vergilplus.sql
@@ -60,7 +60,7 @@ CREATE TABLE `sentiment` (
   `review` text NOT NULL,
   `score` text NOT NULL,
   `magnitude` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
