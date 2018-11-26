db2faace5af8b1fd359c45dce484f6b32b2783eb
pow25 <zhangchi8518@gmail.com>
2018-11-25 22:09:55 -0500

    update .sql

--- data/vergilplus.sql
+++ data/vergilplus.sql
@@ -64,7 +64,7 @@ CREATE TABLE `sentiment` (
   `words` text,
   KEY `idx` (`number`),
   KEY `idx2` (`professor`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
